6431d2fff5228441b32d00b9fac46c179091f665
King-Keith1 <keith4321ben@gmail.com>
2025-07-09 09:55:07 +0200

    commit

--- Mern_Stack/JWT/parole/server/index.js
+++ Mern_Stack/JWT/parole/server/index.js
@@ -1,5 +1,28 @@
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_URL, {});
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-app.use(cookieParser());
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MONGO_URL, PORT } = process.env;
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:4000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
