e90c6eba5ffc20b3593a5196840f529cb0c4f7d1
King-Keith1 <keith4321ben@gmail.com>
2025-07-10 09:15:22 +0200

    commit

--- Mern_Stack/JWT/parole/server/Routes/AuthRoute.js
+++ Mern_Stack/JWT/parole/server/Routes/AuthRoute.js
@@ -1,6 +1,12 @@
-const { Signup } = require("../Controllers/AuthController");
-const router = require("express").Router();
+const express = require("express");
+const { Signup, Login, Logout } = require("../Controllers/AuthController");
+const { userVerification } = require("../Middlewares/AuthMiddleware");
+
+const router = express.Router();
 
 router.post("/signup", Signup);
+router.post("/login", Login);
+router.get("/logout", Logout);
+router.post("/", userVerification); // <-- For protected routes (e.g., homepage check)
 
 module.exports = router;
