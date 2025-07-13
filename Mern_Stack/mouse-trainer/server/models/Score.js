867ac20e1ad317e01f0bb7eafcc05803187284e1
King-Keith1 <keith4321ben@gmail.com>
2025-07-13 20:20:05 +0200

    commit

--- Mern_Stack/mouse-trainer/server/models/Score.js
+++ Mern_Stack/mouse-trainer/server/models/Score.js
@@ -4,6 +4,7 @@ const ScoreSchema = new mongoose.Schema({
   username: String,
   score: Number,
   difficulty: String,
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Score', ScoreSchema);
