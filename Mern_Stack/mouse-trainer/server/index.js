c54644a6dc42717a353f5ff1c1ce7f3336980406
King-Keith1 <keith4321ben@gmail.com>
2025-07-14 10:43:12 +0200

    commit

--- Mern_Stack/mouse-trainer/server/index.js
+++ Mern_Stack/mouse-trainer/server/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); // ✅ Add this line
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
 
 const app = express();
+
+// ✅ Enable CORS for all origins (safe for development)
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
@@ -16,4 +20,3 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000/graphql'));
-
